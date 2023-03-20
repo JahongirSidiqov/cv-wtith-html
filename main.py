8cffe1d8e414612af01edcce6020ef3d32dde44f
Jahongir <123203772+JahongirSidiqov@users.noreply.github.com>
2023-03-20 07:36:21 +0000

    add flask

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+if __name__ =='__main__': 
+  app.run(host='0.0.0.0', port=8080,debug=True)
+
