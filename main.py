1a566d6e690869fc7810e92542bd5d6002ef3918
Jahongir <123203772+JahongirSidiqov@users.noreply.github.com>
2023-03-20 07:58:11 +0000

    connected

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
-app = Flask('app')
+from flask import Flask, render_template
+app = Flask('__name__')
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return render_template('html_project.html')
 
 if __name__ =='__main__': 
   app.run(host='0.0.0.0', port=8080,debug=True)
